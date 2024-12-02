86a10faa1a2e8d8d8df0386a711031e8193c1e04
Reignable <joebarrett401@gmail.com>
2024-12-02 08:57:00 +0000

    eslint

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -1,8 +1,8 @@
-import { describe, expect, it } from "vitest";
-import { hello } from "./hello";
+import { describe, expect, it } from 'vitest'
+import { hello } from './hello'
 
-describe("hello", () => {
-  it("Returns the default", () => {
-    expect(hello()).toBe("Hello world!");
-  });
-});
+describe('hello', () => {
+  it('Returns the default', () => {
+    expect(hello()).toBe('Hello world!')
+  })
+})
